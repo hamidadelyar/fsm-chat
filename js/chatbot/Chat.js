bd79d74de348d63fa1d72596c3a112c2bd9dfbd4
hamid.adelyar <hamidadelyar@gmail.com>
2019-03-22 00:01:11 +0000

    cleaned the class

--- js/chatbot/Chat.js
+++ js/chatbot/Chat.js
@@ -1,17 +1,27 @@
 class Chat {
-  state;
-
   constructor() {
     this.state = "greetings";
   }
 
-  setResponse(response) {
+  /**
+   * @description format our response from the chatbot
+   * @param {*} response 
+   * @param {String} event - identifies what state we should go to next from the current state
+   */
+
+  setResponse(response, event) {
+    if(event) this.setState(response.buttons, event);
+
     return {
-      messages: response.messages,
-      buttons: response.buttons
-    };
+        messages: response.messages,
+        buttons: response.buttons
+      };
   }
 
+  /**
+   * @description A finite state machine, representing the orchestration layer of our chatbot
+   * @param {String} event - identifies what state we should go to next from the current state
+   */
   orchestrator(event) {
     let response;
 
@@ -22,35 +32,36 @@ class Chat {
           buttons: ["about", "blog"]
         };
 
-        if (event) this.setState(response.buttons, event);
-
-        return this.setResponse(response);
+        return this.setResponse(response, event);
       case "about":
         response = {
           messages: ["This is my portfolio", "I have the following projects"],
           buttons: ["blog", "greetings"]
         };
 
-        if (event) this.setState(response.buttons, event);
-
-        return this.setResponse(response);
+        return this.setResponse(response, event);
       default:
         response = {
           messages: ["Sorry didn't understand"],
           buttons: ["about"]
         };
 
-        if (event) this.setState(response.buttons, event);
-
-        return this.setResponse(response);
+        return this.setResponse(response, event);
     }
   }
 
+  /**
+   * @description don't change the state, just see what the response should be for the state that we're in
+   */
   getResponse() {
     return this.orchestrator();
   }
 
-  talk(event = null) {
+  /**
+   * @description talk to the chatbot
+   * @param {String} event 
+   */
+  talk(event = '') {
     if (event) {
       this.orchestrator(event);
     }
@@ -58,12 +69,17 @@ class Chat {
     return this.getResponse();
   }
 
+  /**
+   * @description Sets the next state according by checking the possible next states to see if event is in it
+   * @param {Array} nextStates 
+   * @param {String} event 
+   */
   setState(nextStates, event) {
-    // finds the next state
     const nextState = nextStates.find(
       item => item.toLowerCase() === event.toLowerCase()
     );
-    // set state otherwise keep it at current state
+
+    // update the state or set to incomprehension
     if (nextState) this.state = nextState;
     else this.state = "incomprehension";
   }
